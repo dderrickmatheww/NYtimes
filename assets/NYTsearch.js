2c845fb9a1dd98b5b61d8f6f46733d9cdba68ac3
FRED F <FrederickFaour@github.com>
2019-03-23 13:06:21 -0400

    initial upload of starter JS file

---
+++ assets/NYTsearch.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+// variable declaration section
+var queryURL;
+var searchTerm;
+var numOfRecords;
+var startYear;
+var startendYear;
+
+
+
+
+
+$.ajax({
+  url: queryURL,
+  method: "GET";
+
+
+
+
+}).then(function(response) {
+console.log(response);
+
+
+
+
+  
+}
